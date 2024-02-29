9c95172e50dacbf607631522ec89aad58f39b42d
yubin-im <ybyb130@gmail.com>
2024-02-29 16:36:13 +0900

    feat: 첫 화면 로그인(Login.tsx) 화면으로 설정

---
+++ front_assignment/src/components/Login.tsx
@@ -0,0 +1,14 @@
+export const Login = () => {
+  return (
+    <>
+      <header className='flex items-center justify-between p-5 bg-cyan-500 text-white'>
+        <div className='text-xl font-bold'>Hanaro Album</div>
+      </header>
+
+      <div className='flex justify-between p-4'>
+        <div className='mr-4'>User ID...</div>
+        <div>Sign In</div>
+      </div>
+    </>
+  );
+};
