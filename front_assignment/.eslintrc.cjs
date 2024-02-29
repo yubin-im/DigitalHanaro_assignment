ae487e51c33c455bd606bda08800f39dc2c9330f
yubin-im <ybyb130@gmail.com>
2024-02-29 14:20:08 +0900

    eslint 옵션, prettier 추가

--- front_assignment/.eslintrc.cjs
+++ front_assignment/.eslintrc.cjs
@@ -2,17 +2,33 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:jsx-a11y/recommended",
+    "plugin:prettier/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
+  parserOptions: {
+    ecmaFeatures: { jsx: true },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react-refresh", "jsx-a11y", "prettier"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "react/no-array-index-key": "error",
+    "prettier/prettier": "error",
+    quotes: ["error", "single", { allowTemplateLiterals: true }],
   },
-}
+  settings: {
+    react: { version: "detect" }, // for react version warnning
+  },
+};
