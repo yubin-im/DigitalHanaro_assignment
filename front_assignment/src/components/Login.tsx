a01c8ef4eba483351f815bb98139a3248751c1d3
yubin-im <ybyb130@gmail.com>
2024-02-29 17:35:52 +0900

    design: 로그인 화면(Login.tsx) 뷰 생성

--- front_assignment/src/components/Login.tsx
+++ front_assignment/src/components/Login.tsx
@@ -1,14 +1,20 @@
 export const Login = () => {
   return (
     <>
-      <header className='flex items-center justify-between p-5 bg-cyan-500 text-white'>
+      <header className='flex items-center justify-between p-5 bg-cyan-600 text-white'>
         <div className='text-xl font-bold'>Hanaro Album</div>
       </header>
 
       <div className='flex justify-between p-4'>
-        <div className='mr-4'>User ID...</div>
-        <div>Sign In</div>
+        <div className='mr-4'>
+          {/* <input type='number' ref={idRef} /> */}
+          <input type='number' placeholder='User ID...' />
+        </div>
+        <button className='bg-green-400 text-white'>Sign In</button>
       </div>
+
+      {/* Todo: 입력값이 1~10이 아닐 경우만 표시 */}
+      <div className='text-red-500'>User ID는 1~10번만 가능합니다.</div>
     </>
   );
 };
