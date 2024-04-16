af2c1f7b81e924597acc103ba2898386577bb604
yubin-im <ybyb130@gmail.com>
2024-04-16 16:59:59 +0900

    백엔드 과제: 프로젝트 생성

---
+++ back_assignment/src/main/java/com/assignment/back_assignment/BackAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.back_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackAssignmentApplication.class, args);
+    }
+
+}
