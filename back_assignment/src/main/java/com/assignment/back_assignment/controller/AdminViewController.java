5ec1b3758bd8bb210f151fb8a6ce583d1e592b5a
yubin-im <ybyb130@gmail.com>
2024-04-17 17:33:39 +0900

    feat: 사용자, 관리자 페이지 ViewController 구현

---
+++ back_assignment/src/main/java/com/assignment/back_assignment/controller/AdminViewController.java
@@ -0,0 +1,38 @@
+package com.assignment.back_assignment.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AdminViewController {
+    // 관리자 로그인 화면
+    @GetMapping("/admin")
+    public String viewAdminLogin() {
+        return "admin/admin_login";
+    }
+
+    // 회원관리 화면
+    @GetMapping("/admin-member")
+    public String viewAdminMember() {
+        return "admin/admin_member";
+    }
+
+    // 공지사항관리 화면
+    @GetMapping("/admin-notice")
+    public String viewAdminNotice() {
+        return "admin/admin_notice";
+    }
+
+    // 공지사항관리- 상세 화면
+    @GetMapping("/admin-notice-view")
+    public String viewAdminNoticeView(@RequestParam Long noticeIdx) {
+        return "admin/admin_notice_view";
+    }
+
+    // 공지글쓰기 화면
+    @GetMapping("/admin-notice-write")
+    public String viewAdminNoticeWrite() {
+        return "admin/admin_notice_write";
+    }
+}
---
+++ back_assignment/src/main/java/com/assignment/back_assignment/controller/MainViewController.java
@@ -0,0 +1,74 @@
+package com.assignment.back_assignment.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainViewController {
+    // 메인 화면
+    @GetMapping("/")
+    public String viewMain() {
+        return "index";
+    }
+
+    // 로그인 화면
+    @GetMapping("/member/login")
+    public String viewLogin(){
+        return "member/login";
+    }
+
+    // 회원가입 화면
+    @GetMapping("/member/join")
+    public String viewJoin(){
+        return "member/join2";
+    }
+
+    // 아이디찾기 화면
+    @GetMapping("/idFind")
+    public String viewFindId(){
+        return "member/idFind";
+    }
+
+    // 비밀번호찾기 화면
+    @GetMapping("/passwordFind")
+    public String viewFindPwd(){
+        return "member/passwordFind";
+    }
+
+    // 공지사항 화면
+    @GetMapping("/community/community01")
+    public String viewAnnounce(){
+        return "community/community01";
+    }
+
+    // 공지사항- 상세 화면
+    @GetMapping("/community/community01_1")
+    public String viewAnnounceDetail(@RequestParam Long noticeIdx){
+        return "community/community01_1";
+    }
+
+    // 1:1문의 화면
+    @GetMapping("/customer/customer01")
+    public String viewInquiry(){
+        return "customer/customer01";
+    }
+
+    // 묻고답하기 화면
+    @GetMapping("/customer/customer02")
+    public String viewQnA(){
+        return "customer/customer02";
+    }
+
+    // 묻고답하기- 상세 비밀번호확인 화면
+    @GetMapping("/customer/customer02_3")
+    public String viewQnADetailPwd(@RequestParam Long qnaIdx){
+        return "customer/customer02_3";
+    }
+
+    // 묻고답하기- 상세 화면
+    @GetMapping("/customer/customer02_4")
+    public String viewQnADetail(){
+        return "customer/customer02_4";
+    }
+}
